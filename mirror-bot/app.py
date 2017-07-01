ec25009006896b6f9bf197d3c19914d12bfda9d8
최현종 <slack_off@naver.com>
2017-07-02 00:23:30 +0900

    re init

--- mirror-bot/app.py
+++ mirror-bot/app.py
@@ -1,39 +1,26 @@
 #-*- coding: utf-8 -*-
 
-## Module Import ##
-from __future__ import print_function
+from flask import Flask, request
 import json
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-import httplib2
-import os
-import datetime
-import requests
-from urlparse import urlsplit
-import urllib2
-from os.path import basename
 
 app = Flask(__name__)
 
-@app.route("//message", methods=['GET', 'POST'])
-@app.route("/message", methods=['GET', 'POST'])
+@app.route('/')
+def hello():
+    return "Hello World!"
 
+@app.route('/keyboard', methods = ["GET", "POST"])
+def key():
+    return """{ "type" : "text" }"""
+
+@app.route('/message', methods = ["GET", "POST"])
 def message():
-    # User Talk Infomation
-    userRequest = json.loads(request.get_data())
-    print('userRequest :', userRequest)
-    insert_type = userRequest['type'].encode('utf-8')
-    print('insert_type :', insert_type)
-    user_key = userRequest['user_key'].encode('utf-8')
-    print('user_key :', user_key)
+    talk_info = json.loads(request.get_data())
+    print('talk_info :', talk_info)
+    content = talk_info['content']
+    print('contet :', content)
 
-    content = userRequest['content'].encode('utf-8')
-    print('content :', content) 
-
-    return """{"message": {"text":""" + '"' + content + '"' + """},"keyboard": {  "type": "text","buttons": """+'["'+'","'.join(firstMenu)+'"]'+""" }}"""
-    
-      
-@app.route("//keyboard", methods=['GET', 'POST'])
-@app.route("/keyboard", methods=['GET', 'POST'])
+    return """{ "message" : { "text" :""" + '"' + content + '"' + """}}"""
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    app.run()
